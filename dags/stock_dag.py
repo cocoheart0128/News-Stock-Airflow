2fdde72625fce2a0c0d59350eafa00ea4af50f0a
Kexin <qiukexin95@gmail.com>
2025-11-28 13:41:45 +0900

    add sqlite connection

--- dags/stock_dag.py
+++ dags/stock_dag.py
@@ -1,12 +1,82 @@
 from airflow import DAG
-from airflow.assets import load_assets
+from airflow.operators.python import PythonOperator
+
+import yfinance as yf
+import sqlite3
+import pandas as pd
+import pendulum
+from typing import List
+import sqlite3
+
+
+tz = pendulum.timezone('Asia/Seoul')
+start_date = '{{execution_date + macros.timedelta(hours = 9,days=7).strftime("%Y-%m-%d")}}'
+end_date = '{{execution_date + macros.timedelta(hours = 9,days=1).strftime("%Y-%m-%d")}}'
+
+DB_PATH = "/opt/airflow/db/project.db"   # docker 환경 기준
+
+
+def get_conn(path):
+    conn = sqlite3.connect(DB_PATH,timeout=30)
+    cursor = conn.cursor()
+    return conn,cursor
+
+def get_ticker(table_name):
+    conn,cursor = get_conn(DB_PATH)
+    cursor.execute(f"SELECT * FROM {table_name}")
+
+    rows = cursor.fetchall()
+    columns = [description[0] for description in cursor.description]
+    df = pd.DataFrame(rows, columns=columns)
+    conn.close()
+
+    return df['']
+
+
+
+
+def get_stock_data(**context):
+    ticker = "AAPL"  # 삼성전자 예시
+    df = yf.download(ticker, period="1d")  # 오늘 데이터
+    df.reset_index(inplace=True)
+    context['ti'].xcom_push(key='stock_data', value=df.to_json())
+    
+
+def insert_data_sqlite(**context):
+    stock_json = context['ti'].xcom_pull(key='stock_data')
+    df = pd.read_json(stock_json)
+    print(df)
+
+    conn = sqlite3.connect(DB_PATH)
+    df.to_sql("stock_price", conn, if_exists="append", index=False)
+    conn.close()
+
 
 with DAG(
-    dag_id="stock_asset_pipeline",
-    schedule="@daily",
-    start_date="2025-01-01",
-    catchup=False,
-    tags=["stock", "asset", "finance"]
-):
-    # Load asset functions from assets folder
-    load_assets("assets")
+    dag_id="yfinance_to_sqlite",
+    start_date='2025-01-01',
+    schedule_interval="0 9 * * *",  # 매일 09시
+    catchup=False
+) as dag:
+
+    start = PythonOperator(
+        task_id="start",
+        python_callable=lambda: print("Start DAG"),
+    )
+
+    collect_data = PythonOperator(
+        task_id="collect_data",
+        python_callable=get_stock_data,
+    )
+
+    insert_data = PythonOperator(
+        task_id="insert_to_sqlite",
+        python_callable=insert_data_sqlite,
+    )
+
+    end = PythonOperator(
+        task_id="end",
+        python_callable=lambda: print("DAG end"),
+    )
+
+    start >> collect_data >> insert_data >> end
