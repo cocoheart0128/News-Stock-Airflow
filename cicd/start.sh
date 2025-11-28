58dad8759b040dead14f1d11d90ebee12572b7cb
Kexin <qiukexin95@gmail.com>
2025-11-28 17:29:29 +0900

    delete sample dag

--- cicd/start.sh
+++ cicd/start.sh
@@ -1,5 +1,5 @@
 colima start --arch aarch64 --cpu 4 --memory 8 --disk 60
-docker-compose -f cicd/docker-compose.yaml build
+docker-compose -f cicd/docker-compose.yaml build --no-cache
 docker-compose -f cicd/docker-compose.yaml up airflow-init
 docker-compose -f cicd/docker-compose.yaml up -d
 
--- cicd/stop.sh
+++ cicd/stop.sh
@@ -1,4 +1,7 @@
 echo "Stopping Airflow containers..."
-docker-compose -f cicd/docker-compose.yaml down
+docker-compose -f cicd/docker-compose.yaml down --rmi all
+#--rmi all 이미지 삭제
+#-v DB 삭제 및 초기화 조심 사용 해야함
+
 echo "Stopping Colima VM..."
 colima stop
