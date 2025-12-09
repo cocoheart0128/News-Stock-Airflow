b8e80673e252797aa0d91cd9b699a58c1e7a4524
Kexin <qiukexin95@gmail.com>
2025-12-09 10:17:31 +0900

    update schedule

--- dags/asset_asset_test_dag.py
+++ dags/asset_asset_test_dag.py
@@ -1,32 +1,32 @@
-from airflow.sdk import asset
+# from airflow.sdk import asset
 
 
-@asset(schedule="@daily")
-def extracted_data():
-    return {"a": 1, "b": 2}
+# @asset(schedule="@daily")
+# def extracted_data():
+#     return {"a": 1, "b": 2}
 
 
-@asset(schedule=extracted_data)
-def transformed_data(context):
+# @asset(schedule=extracted_data)
+# def transformed_data(context):
 
-    data = context["ti"].xcom_pull(
-        dag_id="extracted_data",
-        task_ids="extracted_data",
-        key="return_value",
-        include_prior_dates=True,
-    )
-    print(data)
-    return {k: v * 2 for k, v in data.items()}
+#     data = context["ti"].xcom_pull(
+#         dag_id="extracted_data",
+#         task_ids="extracted_data",
+#         key="return_value",
+#         include_prior_dates=False,
+#     )
+#     print(data)
+#     return {k: v * 2 for k, v in data.items()}
 
 
-@asset(schedule=transformed_data)
-def loaded_data(context):
+# @asset(schedule=transformed_data)
+# def loaded_data(context):
 
-    data = context["task_instance"].xcom_pull(
-        dag_id="transformed_data",
-        task_ids="transformed_data",
-        key="return_value",
-        include_prior_dates=True,
-    )
-    summed_data = sum(data.values())
-    print(f"Summed data: {summed_data}")
+#     data = context["task_instance"].xcom_pull(
+#         dag_id="transformed_data",
+#         task_ids="transformed_data",
+#         key="return_value",
+#         include_prior_dates=True,
+#     )
+#     summed_data = sum(data.values())
+#     print(f"Summed data: {summed_data}")
